1fc661562a3655453fc15bc24e8b8800fdddee80
Fredrik Kittel-Nielsen <fredrik@Fredrik-sin-MacBook-Air.local>
2026-05-28 20:20:01 +0200

    fix signOut import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import { auth, db } from "./firebase";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updateProfile } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, updateProfile } from "firebase/auth";
 import { collection, addDoc, getDocs, query, orderBy, doc, updateDoc, arrayUnion, deleteDoc, setDoc, getDoc, where } from "firebase/firestore";
 import { BrowserMultiFormatReader } from "@zxing/library";
 
